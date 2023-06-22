5845a9aeb5f3348c1bcca05053623c8ed0e01609
PRASHANT-2922 <prashant.s2922@gmail.com>
2023-06-22 16:49:12 +0530

    Shell Script

--- Shell_Script/script4.sh
+++ Shell_Script/script4.sh
@@ -1,6 +1,6 @@
-awk '/everything normal for id/ {print $NF " " $3 " " $1 " " $2 " - "}' sample_log.txt > answer4_1.txt
-awk '/detail for id/ {print $NF " " $3 " " $1 " " $2 " - "}' sample_log.txt > answer4_2.txt
-awk '/verbose detail/ {print $NF " " $3 " " $1 " " $2 " - "}' sample_log.txt > answer4_3.txt
+awk '/everything normal for id/ {print $NF ", " $3 ", " $1 ", " $2}' sample_log.txt > answer4_1.txt
+awk '/detail for id/ {print $NF ", " $3 ", " $1 ", " $2}' sample_log.txt > answer4_2.txt
+awk '/verbose detail/ {print $NF ", " $3 ", " $1 ", " $2}' sample_log.txt > answer4_3.txt
 # cat answer4_1.txt 
 # cat answer4_2.txt 
 # cat answer4_3.txt 
